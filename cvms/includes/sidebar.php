c2f5acd60db8c1e0fff05624818ca935e1a6f53d
sree <srilakshmiiit@gmail.com>
2020-12-27 11:16:57 +0530

    frontend

---
+++ cvms/includes/sidebar.php
@@ -0,0 +1,34 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h1>RVCE VMS</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: black">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: black">
+                                <i class="fa fa-user"></i>Add a Visitor</a>
+                        </li>
+   
+
+<li>
+                            <a href="manage-newvisitors.php"  style="color: black">
+                                <i class="fa fa-users"></i>Manage Visitors</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: black">
+                                <i class="fas fa-copy"></i>View Reports</a>
+                        </li>  
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
